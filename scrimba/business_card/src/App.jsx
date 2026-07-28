b2663805223cdb7d624607178d0afc3e3c284180
TheSharpOwl <ziadxkabakibi@hotmail.com>
2026-07-28 13:42:49 +0300

    Add initial files for scrimba's react course first solo project

---
+++ scrimba/business_card/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+  return (
+    <div>
+      <h1>Business Card</h1>
+    </div>
+  )
+}
+export default App
---
+++ scrimba/temp/index.js
@@ -0,0 +1,4 @@
+newH1 = document.createElement("h1");
+newH1.textContent = "Test";
+newH1.className = "header";
+document.getElementById("root").appendChild(newH1);
