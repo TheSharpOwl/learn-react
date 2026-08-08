5a35957e9138e0f5f2f9a4918a28893b9818dce9
TheSharpOwl <ziadxkabakibi@hotmail.com>
2026-08-09 00:49:29 +0300

    Add the bottom part

--- scrimba/business_card/src/App.jsx
+++ scrimba/business_card/src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import Top from './components/Top.jsx'
 import Middle from './components/Middle.jsx'
-
+import Bottom from './components/Bottom.jsx'
 
 function App() {
   return (
     <div className="main">
       <Top />
       <Middle />
+      <Bottom />
     </div>
   )
 }
---
+++ scrimba/business_card/src/components/Bottom.jsx
@@ -0,0 +1,20 @@
+import './Bottom.css'
+
+export default function Bottom() {
+    return (
+        <div className="bottom">
+            <div className="bottom-paragraph">
+            <h3>About</h3>
+            <p>I'm a software Engineer with 5 Years of Experience.
+                 Studied Computer Science at Innopolis University 
+                 and got a master's degree. I can speak Arabic, 
+                 English and French</p>
+                 </div>
+
+            <div className="bottom-paragraph">
+            <h3>Interests</h3>
+            <p>Gym, Swimming, Billiards, Ice Cream</p>
+            </div>
+        </div>
+    )
+}
