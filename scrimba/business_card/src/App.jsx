1e4eb3b12d83e7d80fcc6e7efb7fb983450a639b
TheSharpOwl <ziadxkabakibi@hotmail.com>
2026-08-09 11:12:16 +0300

    Finalize the business card project from scrimba

--- scrimba/business_card/src/App.jsx
+++ scrimba/business_card/src/App.jsx
@@ -2,13 +2,17 @@ import './App.css'
 import Top from './components/Top.jsx'
 import Middle from './components/Middle.jsx'
 import Bottom from './components/Bottom.jsx'
+import Footer from './components/Footer.jsx'
 
 function App() {
   return (
     <div className="main">
-      <Top />
-      <Middle />
-      <Bottom />
+      <div className="content">
+        <Top />
+        <Middle />
+        <Bottom />
+      </div>
+      <Footer />
     </div>
   )
 }
