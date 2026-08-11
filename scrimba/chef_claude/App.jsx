b8e10285042c7adbe46bbe4c58cec21e1b0953c3
TheSharpOwl <ziadxkabakibi@hotmail.com>
2026-08-11 12:25:05 +0300

    Add chef claude project initial files

---
+++ scrimba/chef_claude/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header"
+/**
+ * Challenge: Build the Header component in a separate file
+ * and render it here in the App component
+ */
+
+export default function App() {
+  return (
+      <Header />
+  )
+}
