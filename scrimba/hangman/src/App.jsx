3f44ec6bd1b064d12cf3632adc2120a4e938ee65
TheSharpOwl <ziadxkabakibi@hotmail.com>
2026-09-25 03:15:26 +0400

    Add the initial files for assembly end game

---
+++ scrimba/hangman/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+/**
+ * Goal: Build out the main parts of our app
+ * 
+ * Challenge: Add a header with the game title
+ * and description. Startin' out easy 🙂🚶‍➡️
+ */
+
+export default function Hangman() {
+    return (
+        <main>
+        <div className="main-start">
+            <h4> Assembly: Endgame </h4>
+            <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+        </div>
+        </main>
+    )
+}
